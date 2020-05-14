1bdda9f8657613cc65b265705d68e6d563b929bb
mvx <maxrprp@gmail.com>
2020-05-14 17:13:16 +0300

    second commit

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,8 +1,16 @@
 var express = require('express');
 var router = express.Router();
-var ctrlMain = require('../controllers/main');
+//var ctrlMain = require('../controllers/main');
+var ctrlLocations = require('../controllers/locations');
+var ctrlOther = require('../controllers/others');
 
 /* GET home page. */
-router.get('/', ctrlMain.index);
+//router.get('/', ctrlMain.index);
+
+router.get('/',ctrlLocations.homelist);
+router.get('/location',ctrlLocations.locationInfo);
+router.get('/location/review/new',ctrlLocations.addReview);
+
+router.get('/about',ctrlOther.about);
 
 module.exports = router;
